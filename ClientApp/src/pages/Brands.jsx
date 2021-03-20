2376bf1215b593e32bbfdd791097c5398c62dc0e
Mike Morris <eight20eight@gmail.com>
2021-03-20 15:44:32 -0400

    updated the up;oad controller with a migration after making sure it had populated

--- ClientApp/src/pages/Brands.jsx
+++ ClientApp/src/pages/Brands.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import axios from 'axios'
+import { useDropzone } from 'react-dropzone'
 
 export function Brands() {
   const [brandsInfo, setBrandsInfo] = useState([])
