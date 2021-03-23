92c6af516ff6953b04b7ea576ba173d2a05f2b84
Mike Morris <eight20eight@gmail.com>
2021-03-23 11:49:24 -0400

    created footer and updated media queries

---
+++ ClientApp/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+export function Footer() {
+  return (
+    <footer>
+      <p>
+        Created by: <a href="https://github.com/Chall008">Corey Hall</a> and{' '}
+        <a href="https://github.com/Grady828">Mike Morris</a>
+      </p>
+    </footer>
+  )
+}
