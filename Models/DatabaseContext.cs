1014328d529ac28ef8f5d3afba7cccb0565a2e71
Mike Morris <eight20eight@gmail.com>
2021-03-23 18:31:02 -0400

    updated db context.cs file

--- Models/DatabaseContext.cs
+++ Models/DatabaseContext.cs
@@ -45,7 +45,7 @@ namespace MikesHumidor.Models
 
             var connectionParts = Regex.Split(_connection, ":|@|/");
 
-            return $"server={connectionParts[2]};database={connectionParts[4]};User Id={connectionParts[0]};password={connectionParts[1]};port={connectionParts[3]}";
+            return $"server={connectionParts[2]};SSL Mode=Require;Trust Server Certificate=true;database={connectionParts[4]};User Id={connectionParts[0]};password={connectionParts[1]};port={connectionParts[3]}";
         }
     }
 }
